c36cf092fc63aff7f4cec45bad5306e81353c63c
Ram Iyer <riyer@teladochealth.com>
2022-02-21 15:14:48 -0500

    commenting syntax check

--- kafka/setup.sh
+++ kafka/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # ansible zoo_servers -i host_list -m ping
-ansible-playbook -i host_list zoo.yml --syntax-check
+# ansible-playbook -i host_list zoo.yml --syntax-check
 ansible-playbook -i host_list zoo.yml
 # ansible zoo_servers -i host_list zoo.yml
