4907d40ff975de188109aa3596270ff69f397319
Ram Iyer <riyer@teladochealth.com>
2022-02-21 15:08:42 -0500

    correcting playbook with hosts

--- kafka/setup.sh
+++ kafka/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # ansible zoo_servers -i host_list -m ping
-ansible-playbook zoo_servers -i host_list zoo.yml  --syntax-check
+ansible-playbook -i host_list zoo.yml --syntax-check
+ansible-playbook -i host_list zoo.yml
 # ansible zoo_servers -i host_list zoo.yml
